1ee4b210920e0db14864603741295bef17c698ef
Augusto Goncalves <augusto.goncalves@autodesk.com>
2016-07-25 19:51:33 -0300

    handling questions by email

--- server.js
+++ server.js
@@ -7,32 +7,50 @@ var htmlToText = require('html-to-text');
 
 // SETUP: zendesk
 var zendeskclient = zendesk.createClient({
-    username: 'YOUR ZENDESK EMAIL ACCOUNT',
-    token: 'YOUR ZENDESK TOKEN',
-    remoteUri: 'https://YOUDOMAIN.zendesk.com/api/v2',
+    username: 'your user name here',
+    token: 'your token here',
+    remoteUri: 'https://YOUR_DOMAIN.zendesk.com/api/v2',
+    //debug: true
 });
 
 // SETUP: Stackoverflow
 // 1. tags being monitored
-var TAGS = 'semi-colon list of tags to monitor';
+var TAGS = [
+    'autodesk-forge',
+    'autodesk-data-management',
+    'autodesk-model-derivative',
+    'autodesk-viewer',
+    'autodesk-designautomation',
+    'autodesk-3d-print'
+];
 // 2. developer key
-var STACKOVERFLOW_DEVELOPER_KEY = 'YOUR STACKOVERFLOW KEY (PUBLIC)'; // this is used for quota
-// 3. checking interval
-var INTERVAL = 10; // runs every minute interval
+var STACKOVERFLOW_DEVELOPER_KEY = 'your key here'; // this is used for quota (10k per day)
 
-cron.schedule('*/' + INTERVAL + ' * * * *', function () {
+// 3. email alias of the portal (form)
+var PORTAL_EMAIL_ALIAS = 'EMAIL FORM ALIAS';
+
+// get new questions from stackoverflow every 10 minutes
+cron.schedule('0,10,20,30,40,50 * * * *', function () {
+    console.log('Searching for new questions - ' + (new Date()).toString());
     getNewQuestions();
 });
 
-function getNewQuestions() {
-    console.log('Searching for new questions - ' + (new Date()).toString());
+// adjust questions from portal form every aprox. 3 minutes (10 calls per minute limitation)
+cron.schedule('3,5,8,13,15,17,23,25,27,33,35,37,43,45,47,53,55,57 * * * *', function () {
+    console.log('Analyzing tickets from portal - ' + (new Date()).toString());
+    adjustQuestionsFromForgePortal();
+});
 
-    // time range: 5 days back
+function getNewQuestions() {
+    // time range: 1 hour back
+    // if we take several questions, then we need to check them all
+    // on Zendesk, but there is a rate limit for calling Zendesk API
+    // https://developer.zendesk.com/rest_api/docs/core/introduction#rate-limits
     var now = new Date();
-    var before = new Date(now - 60000 * 60 * 24 * 5);
+    var before = new Date(now - 60000 * 60 * 1 /*hour*/);
 
     // create a list of tags
-    var listOfTags = TAGS.split(';');
+    //var listOfTags = TAGS.split(';');
 
     // stackoverflow return questions for 1 tag at a time
     // is we pass 2 or more tags, will return questions with all tags
@@ -40,7 +58,7 @@ function getNewQuestions() {
     // monitored tags. once all requests are done, proccess them
     var listOfQuestions = {};
     var pendingRequests = 0;
-    listOfTags.forEach(function (tag, index) {
+    TAGS.forEach(function (tag, index) {
         var url = 'https://api.stackexchange.com/2.2/questions?fromdate=' + Math.floor(before.getTime() / 1000)
             + '&todate=' + Math.floor(now.getTime() / 1000)
             + '&order=desc&sort=activity&tagged=' + tag
@@ -48,7 +66,7 @@ function getNewQuestions() {
 
         pendingRequests++;
         makeRequest(url, function (res) {
-            if (res.items != null) {
+            if (res != null && res.items != null) {
                 res.items.forEach(function (question) {
                     listOfQuestions[question.question_id] = question;
                 });
@@ -65,8 +83,7 @@ function getNewQuestions() {
 function processQuestions(questions) {
     for (var id in questions) {
         (function (question) { // this access mutable variable from closure
-            var title = question.title;
-            var body = question.body;
+            console.log('\tChecking question ' + question.question_id);
             var stackuser = question.owner;
             // get or create zendesk user for this question
             getZendeskUser(stackuser, function (zendeskuser) {
@@ -84,7 +101,7 @@ function processQuestions(questions) {
 function getZendeskUser(stackuser, onuser) {
     // first check if the user exists...
     zendeskclient.users.search({external_id: stackuser.user_id}, function (err, req, users) {
-        if (users.length == 0) {
+        if (users != null && users.length == 0) {
             // if not, then create
             // (use stackoverflow user ID as zendesk end-user external ID
             var user = {
@@ -100,19 +117,20 @@ function getZendeskUser(stackuser, onuser) {
                 }
             };
             zendeskclient.users.create(user, function (err, req, newuser) {
-                console.log('New end-user for user ' + stackuser.user_id);
+                console.log('\tNew end-user for user ' + stackuser.user_id);
                 onuser(newuser);
             });
         }
-        else
-            onuser(users[0]);
+        else {
+            onuser(users != null ? users[0] : null);
+        }
     });
 }
 
 function createZendeskTicket(stackquestion, zendeskuser, onnewticket) {
     // check if the question is already logged (as a ticket)
     zendeskclient.tickets.show({external_id: stackquestion.question_id}, function (err, req, tickets) {
-        if (tickets.length == 0) {
+        if (tickets != null && tickets.length == 0) {
             // if not, create a new ticket with the question information
             // (use stackoverflow question ID as zendesk ticket external ID
             var ticket =
@@ -123,7 +141,7 @@ function createZendeskTicket(stackquestion, zendeskuser, onnewticket) {
                         "html_body": // this header will remember agents to not reply on zendesk UI
                         '<h3><span style="color: red">IMPORTANT:</span> answer at '
                         + '<a href="' + stackquestion.link + '" target="_blank">Stackoverflow</a></h3>'
-                        + '-----------------------------------------------------------------------------------'
+                        + '-----------------------------------------'
                         // the actual question body
                         + stackquestion.body,
                     },
@@ -138,12 +156,13 @@ function createZendeskTicket(stackquestion, zendeskuser, onnewticket) {
                 }
             };
             zendeskclient.tickets.create(ticket, function (err, req, newticket) {
-                console.log('New ticket for question ' + stackquestion.question_id);
+                console.log('\tNew ticket for question ' + stackquestion.question_id);
+                if (err != null) console.log(err);
                 onnewticket(newticket);
             });
         }
         else
-            onnewticket(tickets[0]);
+            onnewticket(tickets == null ? null : tickets[0]);
     });
 }
 
@@ -154,16 +173,89 @@ function makeRequest(url, onsuccess) {
         method: 'GET',
         gzip: true,
     }, function (error, response, body) {
-        if (error != null) console.log(error); // connection problems
-        body = JSON.parse(trim(body));
-        if (body.errors != null || response.statusCode != 200) {
+        if (error != null) {
+            console.log(error);
+            onsuccess(null);
+        } // connection problems
+
+        if (body == null || body.errno != null || response.statusCode != 200) {
             console.log(body.quota_remaining);
             console.log(body.errors);
         }
+        if (body.errno == null)
+            body = JSON.parse(body);
+        //console.log(body.quota_remaining);
         onsuccess(body);
     })
 }
 
-// first run
-console.log('Running every ' + INTERVAL + ' minutes for tags: ' + TAGS);
-getNewQuestions();
+// this function will get questions that come from the Forge Dev Portal (by email) and change
+// the requester to the email that sent the question, otherwise the replies will go to forge email alias
+function adjustQuestionsFromForgePortal() {
+    zendeskclient.search.query("type:ticket status:new requester:" + PORTAL_EMAIL_ALIAS, function (err, req, tickets) {
+        tickets.forEach(function (ticket, index) {
+            // now the ticket is recoverd, let's change the requested ID based on the
+            // email of the user who sent the question
+            // first let's "parse" the body of the request
+            var props = {};
+
+            var params = ticket.description.split('\n');
+            params.forEach(function (param, index) {
+                if (index < 3) { // just name, email, API
+                    param = param.split(':');
+                    props[trim(param[0]).replace(/[\'_ ]/g, '')] = trim(param[1]);
+                }
+            });
+
+            // now check if there is a user with this email account
+            var query = 'type:user email:' + props['UsersEmail'];
+            zendeskclient.users.search({query: query}, function (err, req, users) {
+                var emailuser = (users != null && users.length > 0 ? users[0] : null);
+
+                // if not user yet, let's create it
+                if (emailuser == null) {
+                    var user = {
+                        'user': {
+                            'name': props['UsersName'],
+                            'role': 'end-user',
+                            'verified': 'true',
+                            'email': props['UsersEmail'],
+                        }
+                    };
+                    zendeskclient.users.create(user, function (err, req, newuser) {
+                        if (err!=null){
+                            console.log(err);
+                            return;
+                        }
+                        console.log('\tNew end-user for user ' + props['UsersEmail']);
+                        emailuser = newuser;
+                    });
+                }
+
+                // now that we have the user, let's update the ticket
+                if (emailuser != null) {
+                    var tags = [];
+                    tags.push(props['WhichAPI'].replace(/[ ]/g, '-').toLowerCase());
+                    var updateTicketInfo = {
+                        "ticket": {
+                            "requester_id": emailuser.id,
+                            "tags": tags,
+                        }
+                    };
+
+                    zendeskclient.tickets.update(ticket.id, updateTicketInfo, function (err, req, updatedTicket) {
+                        if (updatedTicket == null) {
+                            console.log(err);
+                            return;
+                        }
+
+                        if (err == null)
+                            console.log('Form ticket ' + updatedTicket.id + ' adjusted for user: ' + emailuser.email);
+                    });
+                }
+            });
+        });
+    });
+}
+
+console.log('Running for tags: ' + TAGS.join(';'));
